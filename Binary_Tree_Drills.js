ab6a02bd2754964189b67da916a2786ba75322de
R-Cole <randy@goodglue.com>
2019-01-23 10:25:11 -0700

    begin

--- Binary_Tree_Drills.js
+++ Binary_Tree_Drills.js
@@ -4,25 +4,42 @@
 
 //3,1,4,6,9,2,5,7
 
-//         6
+//         3
 //        / \
-//       5   7
-//      /     \
-//     3       9
-//    /\
-//   2  4
-//  /
-// 1
-//          
-//          
+//       1   4
+//      /    \
+//     2      5
+//             \
+//             6
+//              \
+//              7
+//               \
+//                9
 //         
-//       5   
-//      / \    
-//     3   7  
-//    /\    \
-//   2  4   9
-//  /
-// 1
+//         4
+//        / \
+//       1   5
+//      /     \
+//     2      6
+//             \
+//             7
+//              \
+//              9
+
+//Tree Class
+class BinarySearchTree {
+
+  constructor(key=null, value=null, parent=null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+}
+
+
 
 
 
