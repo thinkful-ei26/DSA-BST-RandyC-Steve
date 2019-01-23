0fdcbeab586ac29c66db61a600dc9a42037bb2b5
R-Cole <randy@goodglue.com>
2019-01-23 12:46:18 -0700

    more

--- Binary_Tree_Drills.js
+++ Binary_Tree_Drills.js
@@ -154,8 +154,56 @@ class BinarySearchTree {
 
   }
 
+  //replace with a new node
+  replaceWith(node){
 
+    if(this.parent) {
 
+      if(this === this.parent.left){
+
+        this.parent.left = node;
+
+      }
+      else if (this === this.parent.right){
+        this.parent.right.node;
+      }
+      if(node){
+        node.parent = this.parent;
+      }
+
+    }
+    else{
+
+      if(node){
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else{
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+
+    }
+
+  }
+
+  //find minimum key on a tree
+  _findMin(){
+
+    if(!this.left){
+
+      return this;
+
+    }
+
+    return this.left._findMin();
+ 
+  }
+ 
 }
   
   
